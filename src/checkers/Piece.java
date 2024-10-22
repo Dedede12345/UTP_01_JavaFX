8b5a09b5dbf375ef8fb8c02440518b8c1e0baa5d
Dedede12345 <gleb@adenisov.com>
2024-10-22 18:21:00 +0200

    Final Java only stage

--- src/checkers/Piece.java
+++ src/checkers/Piece.java
@@ -6,10 +6,12 @@ import javafx.scene.shape.Ellipse;
 import static checkers.CheckersApp.TILE_SIZE;
 public class Piece extends StackPane {
     private PieceType type;
+    private double mouseX, mouseY;
+    private double oldX, oldY;
     public Piece(PieceType type, int x, int y){
         this.type = type;
 
-        relocate(x * TILE_SIZE, y * TILE_SIZE);
+        move(x, y);
 
         Ellipse bg = new Ellipse(TILE_SIZE * 0.3125, TILE_SIZE * 0.26);
         bg.setFill(Color.BLACK);
@@ -17,7 +19,7 @@ public class Piece extends StackPane {
         bg.setStrokeWidth(TILE_SIZE * 0.03);
 
         bg.setTranslateX((TILE_SIZE - TILE_SIZE * 0.3125 * 2) / 2);
-        bg.setTranslateY((TILE_SIZE - TILE_SIZE * 0.26 * 2) / 2);
+        bg.setTranslateY((TILE_SIZE - TILE_SIZE * 0.26 * 2) / 2 + TILE_SIZE * 0.07);
 
         Ellipse ellipse = new Ellipse(TILE_SIZE * 0.3125, TILE_SIZE * 0.26);
         ellipse.setFill(type == PieceType.RED ? Color.valueOf("c40003") : Color.valueOf("#fff9f4"));
@@ -25,9 +27,42 @@ public class Piece extends StackPane {
         ellipse.setStrokeWidth(TILE_SIZE * 0.03);
 
         ellipse.setTranslateX((TILE_SIZE - TILE_SIZE * 0.3125 * 2) / 2);
-        ellipse.setTranslateY((TILE_SIZE - TILE_SIZE * 0.26 * 2) / 2 + TILE_SIZE * 0.07);
+        ellipse.setTranslateY((TILE_SIZE - TILE_SIZE * 0.26 * 2) / 2);
 
 
         getChildren().addAll(bg, ellipse);
+
+        setOnMousePressed(e -> {
+            mouseX = e.getSceneX();
+            mouseY = e.getSceneY();
+        });
+
+        setOnMouseDragged(e -> {
+            relocate(e.getSceneX() - mouseX + oldX, e.getSceneY() - mouseY + oldY);
+        });
+
+
+    }
+
+    public PieceType getType() {
+        return type;
+    }
+
+    public double getOldX() {
+        return oldX;
+    }
+
+    public double getOldY() {
+        return oldY;
+    }
+
+    public void abortMove(){
+        relocate(oldX, oldY);
+    }
+
+    public void move(int x, int y){
+        oldX = x * TILE_SIZE;
+        oldY = y * TILE_SIZE;
+        relocate(oldX, oldY);
     }
 }
